19f81ddb71714c2424062d4d8ad8751d54a53999
himanshu <hmishra.gaya@gmail.com>
2024-06-01 22:55:32 +0530

    Float related changes

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ CSV_FILE = 'trades.csv'
 if not os.path.isfile(CSV_FILE):
     with open(CSV_FILE, 'w', newline='') as file:
         writer = csv.writer(file)
-        writer.writerow(['ticker', 'buy_strike_price', 'buy_real_price', 'sell_real_price',
+        writer.writerow(['order_id', 'ticker', 'buy_strike_price', 'buy_real_price', 'sell_real_price',
                          'buy_time', 'sell_time', 'order_type'])
 
 
@@ -28,22 +28,22 @@ def convert_utc_to_ist(utc_time_str):
     return ist_time.strftime("%Y-%m-%d %H:%M:%S")
 
 
-def log_trade(ticker, buy_price, actual_price, buy_time, order_type):
+def log_trade(order_id, ticker, buy_price, actual_price, buy_time, order_type):
     with open(CSV_FILE, 'a', newline='') as file:
         writer = csv.writer(file)
-        writer.writerow([ticker, buy_price, actual_price, '', buy_time, '', order_type])
+        writer.writerow([order_id, ticker, buy_price, actual_price, '', buy_time, '', order_type])
 
 
-def update_trade(ticker, sell_price, sell_time):
+def update_trade(order_id, sell_price, sell_time):
     rows = []
     updated = False
     with open(CSV_FILE, 'r') as file:
         reader = csv.reader(file)
         header = next(reader)
         for row in reader:
-            if row[0] == ticker and row[3] == '' and not updated:
-                row[3] = sell_price
-                row[5] = sell_time
+            if row[0] == order_id and row[4] == '' and not updated:
+                row[4] = sell_price
+                row[6] = sell_time
                 updated = True
             rows.append(row)
 
@@ -62,18 +62,19 @@ def webhook():
     ticker = data.get('ticker')
     timenow_utc = data.get('timenow')
     timenow_ist = convert_utc_to_ist(timenow_utc)
+    order_id = data.get('order_id')
     actual_price = float(data.get('strategy.order.price'))
     action = data.get('strategy.order.action')
     comment = data.get('strategy.order.comment')
 
-    if action == "buy":
+    if comment == 'BuyCE' or comment == "BuyPE":
         if comment == 'BuyCE':
-            price = int(int(actual_price/100)*100)
+            price = int(int(actual_price / 100) * 100)
         else:
-            price = int((int(actual_price/100)+1) * 100)
-        log_trade(ticker, price, actual_price, timenow_ist, comment)
+            price = int((int(actual_price / 100) + 1) * 100)
+        log_trade(order_id, ticker, price, actual_price, timenow_ist, comment)
     else:
-        update_trade(ticker, actual_price, timenow_ist)
+        update_trade(order_id, actual_price, timenow_ist)
 
     return jsonify({'status': 'success'}), 200
 
